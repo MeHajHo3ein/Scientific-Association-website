191a662625e6ef780f0f94afa0045a795edc6c5c
Ho3ein <mehajho3ein@gmail.com>
2026-04-26 17:58:22 +0330

    feat: add images and show Error pages

--- app/Views/errors/403.php
+++ app/Views/errors/403.php
@@ -1,19 +1,19 @@
 <?php
-$pageTitle = 'Error 404';
+$pageTitle = 'Error 403';
 include '../app/Views/layouts/header.php';
 ?>
 
 <div class="container-fluid mb-5 mt-0">
   <div class="row d-flex justify-content-center align-items-center">
-    <img src="/assets/img/404.png" alt="404-IMG" class="w-auto d-block" />
+    <img src="/assets/img/403.png" alt="403-IMG" class="w-auto d-block" />
   </div>
   <div class="row d-flex justify-content-center align-items-center">
     <div class="col-12 text-justify text-center">
-      <h1 class="">404</h1>
-      <h2 class="">اوه! به نظر می‌رسد گم شدی</h2>
-      <h2>صفحه‌ای که دنبالش هستی وجود ندارد.</h2>
+      <h1 class="">403</h1>
+      <h2 class="text-danger">دسترسی محدود!</h2>
+      <h2>این صفحه فقط برای کاربران مجاز در دسترس است.</h2>
       <h2>
-        بیا برگردیم به صفحه
+        برگرد به صفحه
         <a href="index.html" class="link-dark text-primary"> اصلی </a>
       </h2>
     </div>
