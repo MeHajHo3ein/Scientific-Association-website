bc934fb29fc9eeb90d1cec912aec7630efaa0948
Ho3ein <mehajho3ein@gmail.com>
2026-05-02 12:06:11 +0330

    feat: add layouts for header & footer and sidebar

---
+++ app/Views/layouts/dashboard/header.php
@@ -0,0 +1,19 @@
+<?php
+$pageTitle = $pageTitle ?? 'انجمن علمی کامپیوتر دانشگاه خوارزمی (شهرضا)';
+?>
+
+<!doctype html>
+<html lang="fa" dir="rtl">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://lib.arvancloud.ir/font-awesome/6.3.0/css/all.css" />
+  <link rel="stylesheet" href="/assets/css/bootstrap.rtl.min.css" />
+  <link rel="stylesheet" href="/assets/css/styles.css" type="text/css" />
+  <link rel="stylesheet" href="/assets/css/admin.css" />
+  <link rel="icon" href="/assets/img/ico.png" />
+  <title><?= htmlspecialchars($pageTitle); ?></title>
+</head>
+
+<body class="bg-secondary-subtle">
---
+++ app/Views/layouts/dashboard/sidebar.php
@@ -0,0 +1,74 @@
+<!--Side-Bar-Btn-->
+<nav class="navbar navbar-dark bg-secondary navbar-expand-xxl d-xxl-none px-3">
+  <button
+    class="navbar-toggler"
+    type="button"
+    data-bs-toggle="offcanvas"
+    data-bs-target="#sidebar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+</nav>
+<div class="container-fluid">
+  <!--Side-Bar-->
+  <div class="row">
+    <div
+      class="offcanvas-xxl offcanvas-start bg-secondary text-white"
+      tabindex="-1"
+      id="sidebar">
+      <div class="offcanvas-header d-md-none">
+        <h5 class="offcanvas-title">منو</h5>
+        <button
+          type="button"
+          class="btn-close text-reset"
+          data-bs-dismiss="offcanvas"></button>
+      </div>
+      <div class="offcanvas-body p-3">
+        <ul class="navbar-nav">
+          <h5 class="text-light p-3 text-center">پنل مدیریت</h5>
+          <li class="nav-item">
+            <a
+              href="/"
+              class="text-start btn my-1 btn-outline-light py-2 w-100 active">برگشت</a>
+          </li>
+          <li class="nav-item">
+            <button class="text-start btn my-1 btn-outline-light py-2 w-100">
+              ادمین ها
+            </button>
+          </li>
+          <li class="nav-item">
+            <button class="text-start btn my-1 btn-outline-light py-2 w-100">
+              دانشجویان
+            </button>
+          </li>
+          <li class="nav-item">
+            <button class="text-start btn my-1 btn-outline-light py-2 w-100">
+              مدرسین
+            </button>
+          </li>
+          <li class="nav-item">
+            <button class="text-start btn my-1 btn-outline-light py-2 w-100">
+              دوره ها
+            </button>
+          </li>
+          <li class="nav-item">
+            <button class="text-start btn my-1 btn-outline-light py-2 w-100">
+              مقاله ها
+            </button>
+          </li>
+          <li class="nav-item">
+            <button class="text-start btn my-1 btn-outline-light py-2 w-100">
+              فایل های آموزش آفلاین
+            </button>
+          </li>
+          <li class="nav-item">
+            <button class="text-start btn my-1 btn-outline-light py-2 borde w-100">
+              اعلانات
+            </button>
+          </li>
+          <li class="nav-item">
+            <button class="btn my-1 btn-danger text-light w-100">خروج</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
