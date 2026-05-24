9f6c6c2e14938dbaf3e1e7585d22204c3d4562a5
Ho3ein <mehajho3ein@gmail.com>
2026-05-24 10:35:26 +0330

    feat: fetch latest 3 articles from database on homepage

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controllers;
 
+use App\Models\Article;
+
 class HomeController
 {
   // Display homepage view
   public function index()
   {
+    $articleModel = new Article();
+    $homeArticles = $articleModel->getPublishedArticles(3);
     require_once '../app/Views/home/index.php';
   }
 }
