dfe9d4e1fc57d8e681481445dfc9cbbf51dd74a0
Ho3ein <mehajho3ein@gmail.com>
2026-06-06 10:53:43 +0330

    feat: add sidebar for owner

--- app/Views/layouts/dashboard/sidebar.php
+++ app/Views/layouts/dashboard/sidebar.php
@@ -23,7 +23,91 @@
           data-bs-dismiss="offcanvas"></button>
       </div>
       <div class="offcanvas-body p-3">
-        <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
+        <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'owner'): ?>
+          <ul class="navbar-nav">
+            <h5 class="text-light p-3 text-center">پنل مدیریت</h5>
+            <li class="nav-item">
+              <a
+                href="<?= getBackButtonUrl(); ?>"
+                class="text-start btn my-1 btn-outline-light py-2 w-100 <?= isActiveRoute('/panel') ? 'active' : '' ?>">
+                <?= getBackButtonText(); ?>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a
+                href="/panel/admins"
+                class="text-start btn my-1 btn-outline-light py-2 w-100 <?= (isActiveRoute('/panel/admins') || isActiveRoute('/panel/admins/create') || strpos($_SERVER['REQUEST_URI'], '/panel/admins/edit/') === 0) ? 'active' : ''; ?>">
+                مدیران شبکه
+              </a>
+            </li>
+            <li class="nav-item">
+              <a
+                href="/panel/students"
+                class="text-start btn my-1 btn-outline-light py-2 w-100 <?= isActiveRoute('/panel/students') ? 'active' : '' ?>">
+                دانشجویان
+              </a>
+            </li>
+            <li class="nav-item">
+              <a
+                href="/panel/teachers"
+                class="text-start btn my-1 btn-outline-light py-2 w-100 <?= isActiveRoute('/panel/teachers') ? 'active' : '' ?>">
+                اساتید
+              </a>
+            </li>
+            <li class="nav-item">
+              <a
+                href="/panel/courses"
+                class="text-start btn my-1 btn-outline-light py-2 w-100 <?= isActiveRoute('/panel/courses') ? 'active' : '' ?>">
+                دوره ها
+              </a>
+            </li>
+            <li class="nav-item">
+              <a
+                href="/panel/articles"
+                class="text-start btn my-1 btn-outline-light py-2 w-100 <?= isActiveRoute('/panel/articles') ? 'active' : '' ?>">
+                مقالات
+              </a>
+            </li>
+            <li class="nav-item">
+              <a
+                href="/panel/offline-courses"
+                class="text-start btn my-1 btn-outline-light py-2 w-100 <?= isActiveRoute('/panel/offline-courses') ? 'active' : '' ?>">
+                فایل های قابل دانلود
+              </a>
+            </li>
+            <li class="nav-item">
+              <a
+                href="/panel/notifications"
+                class="text-start btn my-1 btn-outline-light py-2 borde w-100 <?= isActiveRoute('/panel/notifications') ? 'active' : '' ?>">
+                اعلانات
+              </a>
+            </li>
+            <li class="nav-item">
+              <a
+                href="/panel/neas"
+                class="text-start btn my-1 btn-outline-light py-2 w-100 <?= isActiveRoute('/panel/neas') ? 'active' : '' ?>">
+                اخبار/رویداد/اطلاعیه
+              </a>
+            </li>
+            <li>
+              <a
+                href="O-Analytics.html"
+                class="text-start btn my-1 btn-outline-light py-2 w-100">
+                آمار
+              </a>
+            </li>
+            <li>
+              <a
+                href="O-Setting.html"
+                class="text-start btn my-1 btn-outline-light py-2 w-100">
+                تنظیمات سیستم
+              </a>
+            </li>
+            <li class="nav-item">
+              <button class="btn my-1 btn-danger text-light w-100">خروج</button>
+            </li>
+          </ul>
+        <?php elseif (isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
           <ul class="navbar-nav">
             <h5 class="text-light p-3 text-center">پنل مدیریت</h5>
             <li class="nav-item">
