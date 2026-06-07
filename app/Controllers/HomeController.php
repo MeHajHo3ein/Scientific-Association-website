359f22fac39fe624d3e44d43ecbc47e364872b1a
Ho3ein <mehajho3ein@gmail.com>
2026-06-07 21:43:42 +0330

    feat: add latest news, events and announcements to homepage

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Models\Article;
 use App\Models\Course;
+use App\Models\Neas;
 
 class HomeController
 {
@@ -16,6 +17,11 @@ class HomeController
     $courseModel = new Course();
     $homeCourses = $courseModel->getPublishedCourses(3);
     $lastCourse = $courseModel->getLastCourse();
+
+    $neasModel = new Neas();
+    $latestNews = $neasModel->getLatestItemsByCategory('news', 3);
+    $latestEvents = $neasModel->getLatestItemsByCategory('event', 3);
+    $latestAnnouncements = $neasModel->getLatestItemsByCategory('announcement', 3);
 
     require_once '../app/Views/home/index.php';
   }
