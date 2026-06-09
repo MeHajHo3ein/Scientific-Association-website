6c7ba7f4827ebca397cd4966d353bf9a8ccf2d21
Ho3ein <mehajho3ein@gmail.com>
2026-06-09 11:27:55 +0330

    feat: fetch latest 3 offline courses from database on homepage

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use App\Models\Article;
 use App\Models\Course;
 use App\Models\Neas;
+use App\Models\OfflineFile;
 
 class HomeController
 {
@@ -22,6 +23,9 @@ class HomeController
     $latestNews = $neasModel->getLatestItemsByCategory('news', 3);
     $latestEvents = $neasModel->getLatestItemsByCategory('event', 3);
     $latestAnnouncements = $neasModel->getLatestItemsByCategory('announcement', 3);
+
+    $offlineFileModel = new OfflineFile();
+    $latestFiles = $offlineFileModel->getPublishedFiles(3);
 
     require_once '../app/Views/home/index.php';
   }
