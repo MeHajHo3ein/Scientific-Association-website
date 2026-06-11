6a3d8fc70cec46e4c870b5aaa3b697101db52b0e
Ho3ein <mehajho3ein@gmail.com>
2026-06-11 14:05:07 +0330

    feat: make footer dynamic with settings and social media

--- app/Views/partials/main-footer.php
+++ app/Views/partials/main-footer.php
@@ -3,7 +3,7 @@
   <div class="container py-3 border-bottom">
     <div class="row text-white d-flex">
       <div class="col-lg-4 col-sm-12 text-white">
-        <h5>انجمن علمی کامپیوتر</h5>
+        <h5><?= htmlspecialchars(setting('site_name', 'انجمن علمی کامپیوتر')) ?></h5>
         <p class="text-justify">
           انجمن علمی دانشجویان کامپیوتر، برگزارکننده دوره‌ها و رویدادهای علمی و پژوهشی
           در حوزه فناوری اطلاعات و مهندسی کامپیوتر.
@@ -18,13 +18,26 @@
       </div>
       <div class="col-lg-4 col-sm-12 text-center">
         <h5>شبکه‌های اجتماعی</h5>
-        <a class="d-block link-primary text-decoration-none text-white" href="#">اینستاگرام</a>
-        <a class="d-block link-primary text-decoration-none text-white" href="#">تلگرام</a>
-        <a class="d-block link-primary text-decoration-none text-white" href="#">گیت‌هاب انجمن</a>
+        <?php
+        $socialMedias = [];
+        try {
+          $settingModel = new App\Models\Setting();
+          $socialMedias = $settingModel->getAllSocialMedias();
+        } catch (Exception $e) {
+          $socialMedias = [];
+        }
+        ?>
+        <?php if (!empty($socialMedias)): ?>
+          <?php foreach ($socialMedias as $social): ?>
+            <a class="d-block link-primary text-decoration-none text-white mb-1" href="<?= htmlspecialchars($social['link']) ?>" target="_blank"><?= htmlspecialchars($social['name']) ?></a>
+          <?php endforeach; ?>
+        <?php else: ?>
+          <span class="d-block text-white">هیچ شبکه اجتماعی ای تعریف نشده</span>
+        <?php endif; ?>
       </div>
     </div>
   </div>
   <div class="text-white text-center p-3">
-    © انجمن علمی رشته کامپیوتر - دانشگاه خوارزمی(شهرضا)
+    © <?= htmlspecialchars(setting('site_name', 'انجمن علمی رشته کامپیوتر')) ?> - دانشگاه خوارزمی(شهرضا)
   </div>
 </footer>
